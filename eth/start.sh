1669c4685434fa8a8302847827f38d08a7bc22e4
Vladimir Vesely <ivesely@fit.vutbr.cz>
2018-08-30 15:52:44 +0200

    Initial commit

---
+++ eth/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+cd /root/eth-net-intelligence-api
+perl -pi -e "s/XXX/$(hostname)/g" app.json
+/usr/bin/pm2 start ./app.json
+#sleep 3
+#geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
+#sleep 3
+#BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
+#GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
+#geth $GETH_OPTS
+geth  --rpcapi "db,personal,eth,net,web3" --rpccorsdomain='*' --rpc --rpcaddr="0.0.0.0"
